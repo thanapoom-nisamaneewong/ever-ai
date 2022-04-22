c5359c32e588ec7613d8e5807556d4a447362343
thanapoom-nisamaneewong <74890654+thanapoom-nisamaneewong@users.noreply.github.com>
2022-04-22 16:19:17 +0700

    change card to text

--- apps/ocr.py
+++ apps/ocr.py
@@ -2,6 +2,6 @@ import streamlit as st
 import streamlit.components.v1 as components
 def app():
     st.title("OCR Card to Text")
-    st.write("check out this [link](https://card-to-text.herokuapp.com/)")
+    st.write("check out this [link](http://54.185.104.161/)")
     st.title("OCR Form to Text")
     st.write("check out this [link](https://ocr-form.herokuapp.com/)")
