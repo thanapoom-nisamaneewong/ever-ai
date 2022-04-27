a9ab160f24c1b5b4b6410d613937f492c523d296
thanapoom-nisamaneewong <74890654+thanapoom-nisamaneewong@users.noreply.github.com>
2022-04-27 13:34:59 +0700

    Delete speeh.py

--- apps/speeh.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-import streamlit.components.v1 as components
-def app():
-    st.title('Speech Recognition')
-    st.write("check out this [link](https://voiceandtext.herokuapp.com/)")
