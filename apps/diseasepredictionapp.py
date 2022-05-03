f82da849b5ba8125031914a387ddad4acaf970a1
thanapoom-nisamaneewong <74890654+thanapoom-nisamaneewong@users.noreply.github.com>
2022-05-03 16:53:14 +0700

    Update diseasepredictionapp.py

--- apps/diseasepredictionapp.py
+++ apps/diseasepredictionapp.py
@@ -1,10 +1,4 @@
 import streamlit as st
-
+import streamlit.components.v1 as components
 def app():
-    st.title('Diagnosis Prediction based on Clinical Text')
-    st.markdown(' ')
-    st.markdown("<font color='black' size=5 ><b>Chief complaint:</b></font>",unsafe_allow_html=True)
-    st.write("check out this [link](http://54.184.187.240:8501/)")
-    st.markdown(' ')
-    st.markdown("<font color='black' size=5 ><b>Chief complaint with patient profile :</b></font>",unsafe_allow_html=True)
-    st.write("check out this [link](https://ever-diagnosis.herokuapp.com/)")
+    components.iframe("http://54.184.187.240:8501/",width=1050, height=1760, scrolling=True)
