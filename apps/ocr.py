18966be0766895f67f958d760bc52907cabf0a36
thanapoom-nisamaneewong <74890654+thanapoom-nisamaneewong@users.noreply.github.com>
2022-05-03 16:52:22 +0700

    Update ocr.py

--- apps/ocr.py
+++ apps/ocr.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import streamlit.components.v1 as components
 def app():
-    st.title("OCR (Image / Handwriting to Text)")
-    st.write("check out this [link](http://34.221.208.207/)")
+    components.iframe("http://34.221.208.207/",width=1050, height=1170, scrolling=True)
     
